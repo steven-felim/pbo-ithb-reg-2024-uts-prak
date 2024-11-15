d0523971a8fa1ff415f481d09bd293abda64825c
steven-felim <stevenfelim04@gmail.com>
2024-11-15 14:11:30 +0700

    bikin class Vendor

---
+++ src/model/Vendor.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Vendor {
+    private String vendorName;
+    private String serviceType;
+    private double vendorFee;
+
+    public String getVendorDetails() {
+        return "Vendor Name\t: " + vendorName + "\nService Type\t: " + serviceType + "\nVendor Fee\t: " + vendorFee;
+    }
+}
