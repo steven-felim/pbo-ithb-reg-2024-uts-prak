676bae47e2e8d0c453f18cd2c5f7d22cf3cc5d1b
steven-felim <stevenfelim04@gmail.com>
2024-11-15 14:27:38 +0700

    bikin class Ticket

---
+++ src/model/Ticket.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Ticket {
+    private TicketType ticketType;
+    private double ticketPrice;
+    private String ticketNumber;
+
+    public Ticket(TicketType ticketType, double ticketPrice, String ticketNumber) {
+        this.ticketType = ticketType;
+        this.ticketPrice = ticketPrice;
+        this.ticketNumber = ticketNumber;
+    }
+}
